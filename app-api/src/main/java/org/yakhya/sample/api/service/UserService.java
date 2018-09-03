9f9e429e1eced19607890403569a24d29fd76874
Yakhya Dabo <yakhyadabo@gmail.com>
2018-09-03 22:37:26 +0100

    [app-api] Simple service class

---
+++ app-api/src/main/java/org/yakhya/sample/api/service/UserService.java
@@ -0,0 +1,9 @@
+package org.yakhya.sample.api.service;
+
+import org.yakhya.sample.domain.model.User;
+
+import java.util.List;
+
+public interface UserService {
+  public List<User> getUsers();
+}
---
+++ app-api/src/main/java/org/yakhya/sample/api/service/impl/UserServiceImpl.java
@@ -0,0 +1,23 @@
+package org.yakhya.sample.api.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.yakhya.sample.api.service.UserService;
+import org.yakhya.sample.domain.model.User;
+import org.yakhya.sample.domain.repository.UserRepository;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class UserServiceImpl implements UserService {
+
+  @Autowired
+  private UserRepository userRepository;
+
+  @Override
+  public List<User> getUsers() {
+    return userRepository.findAllUser();
+  }
+}
