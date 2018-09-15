da0912db3361923e49b14781ea9bbb145febef57
Yakhya Dabo <yakhyadabo@gmail.com>
2018-09-15 13:23:35 +0100

    [app-db-migration] set Next ID Sequence Value

--- app-db-migration/src/main/resources/sql/v0/01_create_tables.sql
+++ app-db-migration/src/main/resources/sql/v0/01_create_tables.sql
@@ -6,5 +6,17 @@ CREATE TABLE sample_user(
   id SERIAL PRIMARY KEY,
   login VARCHAR(25) NOT NULL,
   password VARCHAR(35) NOT NULL,
-  role VARCHAR(35) NOT NULL
+  first_name VARCHAR(40) NOT NULL,
+  last_name VARCHAR(40) NOT NULL
 );
+
+INSERT INTO sample_user VALUES (2, 'ydabo', 'password_DABO', 'Yakhya','Dabo');
+INSERT INTO sample_user VALUES (4, 'ldiop', 'password_DIOP','Lamine','Diop');
+INSERT INTO sample_user VALUES (6, 'mfall', 'rKrpbSV8X34QHbBv','Makhta','Fall');
+
+--changeset sample-docs:2 dbms:postgresql
+
+-- Initialising sequences to avoid collusion with dummy sample DATA
+
+SELECT setval('sample_user_id_seq', 10000);
+ALTER SEQUENCE sample_user_id_seq OWNED BY sample_user.id;
