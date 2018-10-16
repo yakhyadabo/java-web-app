7a3af423300bab8a0c80b948670fa256acfb4d60
Yakhya Dabo <yakhyadabo@gmail.com>
2018-10-16 10:36:18 +0100

    [app-domain]Add toString to Nationality

--- app-domain/src/main/java/org/yakhya/sample/domain/model/Nationality.java
+++ app-domain/src/main/java/org/yakhya/sample/domain/model/Nationality.java
@@ -4,8 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Builder(toBuilder = true)
+@ToString(of = {"code", "name"})
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
