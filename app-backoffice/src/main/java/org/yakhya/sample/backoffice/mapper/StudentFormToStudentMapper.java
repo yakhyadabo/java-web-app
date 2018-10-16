45aefd8e2b845424b3ddc72100a4f4d4d24da70b
Yakhya Dabo <yakhyadabo@gmail.com>
2018-10-16 17:42:39 +0100

    [app-backoffice]Get nationality by country code in the Mapper

--- app-backoffice/src/main/java/org/yakhya/sample/backoffice/mapper/StudentFormToStudentMapper.java
+++ app-backoffice/src/main/java/org/yakhya/sample/backoffice/mapper/StudentFormToStudentMapper.java
@@ -1,8 +1,10 @@
 package org.yakhya.sample.backoffice.mapper;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.yakhya.sample.backoffice.model.NationalityForm;
+import org.yakhya.sample.backoffice.model.NationalityRow;
 import org.yakhya.sample.backoffice.model.StudentForm;
+import org.yakhya.sample.backoffice.service.NationalityService;
 import org.yakhya.sample.domain.model.Nationality;
 import org.yakhya.sample.domain.model.Student;
 
@@ -11,21 +13,23 @@ import java.util.function.Function;
 @Component
 public class StudentFormToStudentMapper implements Function<StudentForm, Student> {
 
+  @Autowired
+  private NationalityService nationalityService;
+
   @Override
   public Student apply(StudentForm studentForm) {
+
     return Student.builder()
         .firstName(studentForm.getFirstName())
         .lastName(studentForm.getLastName())
         .personalNumber(studentForm.getPersonalNumber())
         .education(studentForm.getEducation())
-        .nationality(of(studentForm.getNationality()))
+        .dateOfBirth(studentForm.getDateOfBirth())
+        .nationality(of(studentForm.getCountryCode()))
         .build();
   }
 
-  private static Nationality of(NationalityForm nationality){
-    return Nationality.builder()
-        .code(nationality.getCode())
-        .name(nationality.getName())
-        .build();
+  private Nationality of(String countryCode){
+    return nationalityService.getByCountryCode(countryCode);
   }
 }
